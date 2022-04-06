e0a4d50e7cf570f0bf805cd1af78340714fbfcef
Denis Sigida <47885010+denis-sigida@users.noreply.github.com>
2022-04-06 13:49:37 +0300

    gffgf

--- Sigida.DotPad/DotPad.View/MainWindow.Designer.cs
+++ Sigida.DotPad/DotPad.View/MainWindow.Designer.cs
@@ -46,6 +46,7 @@
             this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.textBox1.ForeColor = System.Drawing.SystemColors.Menu;
             this.textBox1.Location = new System.Drawing.Point(0, 0);
             this.textBox1.Multiline = true;
