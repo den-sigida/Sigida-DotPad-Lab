2cdace60baf2c39b1582361a356d15e69ad0f563
Denis Sigida <47885010+denis-sigida@users.noreply.github.com>
2022-04-06 16:05:05 +0300

    Add text chenger to status bar

--- Sigida.DotPad/DotPad.View/MainWindow.Designer.cs
+++ Sigida.DotPad/DotPad.View/MainWindow.Designer.cs
@@ -28,45 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.statusBar = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.textBoxRealm = new System.Windows.Forms.TextBox();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusBar.SuspendLayout();
             this.SuspendLayout();
             // 
-            // statusStrip1
+            // statusBar
             // 
-            this.statusStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(80)))), ((int)(((byte)(83)))));
-            this.statusStrip1.Location = new System.Drawing.Point(0, 532);
-            this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
-            this.statusStrip1.Size = new System.Drawing.Size(1067, 22);
-            this.statusStrip1.TabIndex = 0;
-            this.statusStrip1.Text = "statusStrip1";
+            this.statusBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(80)))), ((int)(((byte)(83)))));
+            this.statusBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel3});
+            this.statusBar.Location = new System.Drawing.Point(0, 395);
+            this.statusBar.Name = "statusBar";
+            this.statusBar.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.statusBar.Size = new System.Drawing.Size(703, 22);
+            this.statusBar.TabIndex = 0;
+            this.statusBar.Text = "statusStrip1";
             // 
-            // textBox1
+            // toolStripStatusLabel1
             // 
-            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox1.ForeColor = System.Drawing.SystemColors.Menu;
-            this.textBox1.Location = new System.Drawing.Point(0, 0);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(1067, 532);
-            this.textBox1.TabIndex = 1;
+            this.toolStripStatusLabel1.Font = new System.Drawing.Font("Montserrat SemiBold", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.toolStripStatusLabel1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(73, 17);
+            this.toolStripStatusLabel1.Text = "DotPad: txt";
+            // 
+            // textBoxRealm
+            // 
+            this.textBoxRealm.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.textBoxRealm.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxRealm.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxRealm.Font = new System.Drawing.Font("Montserrat", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxRealm.ForeColor = System.Drawing.SystemColors.Menu;
+            this.textBoxRealm.Location = new System.Drawing.Point(0, 0);
+            this.textBoxRealm.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxRealm.Multiline = true;
+            this.textBoxRealm.Name = "textBoxRealm";
+            this.textBoxRealm.Size = new System.Drawing.Size(703, 395);
+            this.textBoxRealm.TabIndex = 1;
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Font = new System.Drawing.Font("Montserrat SemiBold", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.toolStripStatusLabel2.ForeColor = System.Drawing.SystemColors.Control;
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(66, 17);
+            this.toolStripStatusLabel2.Text = "Ln 0, Col 0";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Font = new System.Drawing.Font("Montserrat SemiBold", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.toolStripStatusLabel3.ForeColor = System.Drawing.SystemColors.Control;
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(41, 17);
+            this.toolStripStatusLabel3.Text = "UTF-8";
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.statusStrip1);
+            this.ClientSize = new System.Drawing.Size(703, 417);
+            this.Controls.Add(this.textBoxRealm);
+            this.Controls.Add(this.statusBar);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "MainWindow";
-            this.Text = "Form1";
+            this.Opacity = 0.95D;
+            this.Text = "DotPad";
+            this.statusBar.ResumeLayout(false);
+            this.statusBar.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -74,8 +111,11 @@
 
         #endregion
 
-        private System.Windows.Forms.StatusStrip statusStrip1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.StatusStrip statusBar;
+        private System.Windows.Forms.TextBox textBoxRealm;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
     }
 }
 
--- Sigida.DotPad/DotPad.View/MainWindow.cs
+++ Sigida.DotPad/DotPad.View/MainWindow.cs
@@ -12,9 +12,58 @@ namespace DotPad.View
 {
     public partial class MainWindow : Form
     {
+        private Notepad _notepad;
         public MainWindow()
         {
             InitializeComponent();
+            _notepad = new Notepad();
+
+            textBoxRealm.TextChanged += StatusBarTextChanger;   
         }
+
+        /// <summary>
+        /// Срабатывает при изменении текста в поле ввода текста блокнота 
+        /// Уставливает параметры для объекта Notepad
+        /// Изменяет текст в status bar
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void StatusBarTextChanger(object sender, EventArgs e)
+        {
+            var text = (TextBox)sender;
+
+            //var space = text.Text.Split(' ').Length - 1;
+            
+            _notepad.CurentLine = text.Lines.Length;
+              
+            if (text.Lines.Length != 0) 
+            {
+                _notepad.CurentColumn = text.Lines[_notepad.CurentLine - 1].Length;
+            }
+         
+            _notepad.Characters = text.Text.Replace(" ","").Length;
+
+            statusBar.Items[1].Text = $"Ln {_notepad.CurentLine}, Col {_notepad.CurentColumn}";         
+        }
+
+
+    }
+
+    class Notepad
+    {
+        private int _currentLine = 1;
+        public int Characters { get; set; }  
+        public int CurentLine
+        {
+            get => _currentLine;
+            set
+            {
+                if (value <= 0)
+                    _currentLine = 1;
+                else
+                    _currentLine = value;
+            }
+        }
+        public int CurentColumn { get; set; }
     }
 }
